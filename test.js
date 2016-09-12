bb5da0aa64b0d9aeb2ea8b8d21b0b1049a9f5253
Omar Vazquez <omar.vazquezac@gmail.com>
2016-09-12 23:41:09 +0000

    First commit

---
+++ test.js
@@ -0,0 +1,9 @@
+var Estimote = require('bleacon').Estimote;
+
+var uuid = 'b9407f30-f5f8-466e-aff9-25556b57fe6d';
+var major = 0;
+var minor = 0;
+Estimote.discover(function(e){
+	console.log(e);
+});
+//Esti.startS
