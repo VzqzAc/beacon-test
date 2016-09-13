22a59132d69d1c190b52a7d9a0069533b661f282
Omar Vazquez <omar.vazquezac@gmail.com>
2016-09-13 17:55:40 -0500

    discoverAll method

--- test.js
+++ test.js
@@ -3,9 +3,6 @@ var Estimote = require('bleacon').Estimote;
 var uuid = 'b9407f30-f5f8-466e-aff9-25556b57fe6d';
 var major = 0;
 var minor = 0;
-(function(){
-	Estimote.discover(function(e){
-		console.log(e);
-	});
-})();
-//Esti.startS
+Estimote.discoverAll(function(e){
+	console.log(e);
+});
